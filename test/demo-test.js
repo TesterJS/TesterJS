adb6d1b76a96e3f2450d21ccb2f5200be6776625
JuanMa Ruiz <ruizjuanma@gmail.com>
2017-08-23 17:17:29 +0200

    Configured mocha and dummy test

---
+++ test/demo-test.js
@@ -0,0 +1,8 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+describe('Demo test', function () {
+    it('should return true', () => {
+        expect(true).to.be.true;
+    });
+});
